b755c11a6136a7977ecd82316c5470bc10a2823d
Ahmed Khater <a_khater55@yahoo.com>
2019-08-08 11:08:00 +0200

    Updates

---
+++ precache-manifest.3927d950f25995adb0d8f7150360a202.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263d3f45176e57abd2444e495d008bdc",
+    "url": "/React-Portfolio-Template/index.html"
+  },
+  {
+    "revision": "47e1ce515dbef72df5fa",
+    "url": "/React-Portfolio-Template/static/css/main.35d63f40.chunk.css"
+  },
+  {
+    "revision": "e7acca0fbb601e392447",
+    "url": "/React-Portfolio-Template/static/js/2.0cf661c6.chunk.js"
+  },
+  {
+    "revision": "47e1ce515dbef72df5fa",
+    "url": "/React-Portfolio-Template/static/js/main.8f4db75b.chunk.js"
+  },
+  {
+    "revision": "4b5d64f8c0d6a6189bbb",
+    "url": "/React-Portfolio-Template/static/js/runtime~main.74411b8d.js"
+  }
+]);
